f5fb543aa4a05d595c73b47b34cd59e412580db7
ictmotion <hoekstra@ictmotion.nl>
2020-03-01 20:50:35 +0100

    remove comment

--- AngularLoginJwt/src/app/services/authentication.service.ts
+++ AngularLoginJwt/src/app/services/authentication.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
 import { Token } from '../models/token';
-//import 'rxjs/add/observable/of';
 
 @Injectable({
   providedIn: 'root'
@@ -21,10 +19,4 @@ export class AuthenticationService {
   {
     localStorage.removeItem('auth_token');
   }
-
-  // //https://stackoverflow.com/questions/38067580/property-of-does-not-exist-on-type-typeof-observable
-  // //https://stackoverflow.com/questions/34660010/create-observablet-from-result
-  // GetMoneyTransactionsObservable() : Observable<Array<MoneyTransaction>> {
-  //   return of(this.moneyTransactionList);
-  // }
 }
